c3f010b5055b2311e12fb712765ff41c6dd9a016
Matthew Lau <lau.cy.matthew@gmail.com>
2021-11-01 22:13:03 +0000

    chore: removed dead code on React Redux toolkit TypeScript scaffolding

--- client/src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-export const store = configureStore({
-    reducer: {},
-    devTools: true
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
